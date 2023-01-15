4e799172cc05181fc7830dae7bf34d9fabd05c57
Arty <arty.slv1984@gmail.com>
2023-01-15 23:35:44 +0300

    Добавьте файлы проекта.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string str = "";
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            while (str != "Пока")
+            {
+                Console.WriteLine("Введите имя и нажмите Enter для продолжения....");
+                str = Console.ReadLine();                
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = str });
+                Console.WriteLine($"Сервер: { reply.Message}");
+            }
+
+        }
+    }
+}
